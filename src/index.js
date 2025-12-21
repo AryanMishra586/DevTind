3ce7609e2067c129ed75effb7d329f4f9a388577
Aryan Mishra <aryanmishra12112003@gmail.com>
2025-12-21 19:09:48 +0530

    created a server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express= require("express")
+
+const app=express()
+
+
+app.use("/test",(req,res)=>{
+    res.send("Kya re madharjaat")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("hello ji")
+})
+
+app.use("/",(req,res)=>{
+    res.send("lund le le")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
