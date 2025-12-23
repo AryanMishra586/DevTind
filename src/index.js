37dc13e309486370b364cd587c48d2cc74046b11
Aryan Mishra <aryanmishra12112003@gmail.com>
2025-12-23 12:45:34 +0530

    Routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ const express= require("express")
 
 const app=express()
 
+app.use("/user",[(req,res,next)=>{
+    console.log("Response 1")
+    next()
+},
+(req,res,next)=>{
+    console.log("Response 2")
+    next();
+},
+(req,res,next)=>{
+    console.log("Response 3")
+    next();
+}],
+(req,res,next)=>{
+    console.log("Response 4")
+    next();
+},
+(req,res,next)=>{
+    console.log("Response 5")
+    // res.send("bye")
+    next();
+}
+)
 
 app.get("/user/:id",(req,res)=>{
     console.log(req.params)
